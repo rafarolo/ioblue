e38c3f1eac42575f921dc960e440f088fe3ed9b8
Rafael M. Rôlo <rafaelrolo.senior@sulamerica.com.br>
2021-02-05 13:46:56 -0300

    Unit test

---
+++ src/test/java/br/com/ioblue/test/ApplicationTests.java
@@ -0,0 +1,14 @@
+package br.com.ioblue.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void FisrtUnitTest() {
+		System.out.println("Unit Test OK");
+	}
+
+}
