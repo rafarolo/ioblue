10a7cd2f960621cb6fea5ac442d7f18b59dae76b
Rafael M. Rôlo <rafaelrolo.senior@sulamerica.com.br>
2021-02-10 14:45:37 -0300

    Unit test for AccountController

--- src/test/java/br/com/ioblue/test/ApplicationTests.java
+++ src/test/java/br/com/ioblue/test/ApplicationTests.java
@@ -1,14 +1,34 @@
 package br.com.ioblue.test;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+import java.net.URL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class ApplicationTests {
 
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate testRestTemplate;
+
+	private static final String API_V1_ACCOUNTS = "/api/v1/accounts";
+
 	@Test
-	void FisrtUnitTest() {
-		System.out.println("Unit Test OK");
+	void controllerTest() throws Exception {
+		String mockUrlAllAccounts = new URL("http://localhost:" + port + API_V1_ACCOUNTS).toString();
+		ResponseEntity<String> response = testRestTemplate.getForEntity(mockUrlAllAccounts, String.class);
+		assertEquals(200, response.getStatusCodeValue());
+		assertNotNull(response.getBody());
 	}
 
 }
